eb4e8540e6f5cee1a351489cfefd69e37a84418a
Minjun Kim <minjun@pi-lab.io>
2024-10-11 12:40:36 +0900

    list structure

---
+++ lisp/Ch2/list-lastpair.rkt
@@ -0,0 +1,16 @@
+#lang racket
+
+(define (list-ref items n)
+    (if (= n 0)
+        (car items)
+        (list-ref (cdr items) (- n 1))))
+
+(define (length items)
+    (if (null? items)
+    0
+    (+ 1 (length (cdr items)))))
+
+(define (last-pair items)
+    (list-ref (cdr items) (- length 1)))
+
+(last-pair (list 23 72 149 34))
