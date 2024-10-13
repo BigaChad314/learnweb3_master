96abcfb26a0034c5d3b6558b675f087ef841ca7f
Minjun Kim <minjun@pi-lab.io>
2024-10-13 15:09:15 +0900

    for each alternative

--- lisp/Ch2/for-each.rkt
+++ lisp/Ch2/for-each.rkt
@@ -1,9 +1,18 @@
 #lang racket
 
+;; // Unless를 활용한 for-each
 (define (for-each f xs)
   (unless (null? xs)
     (f (car xs))
     (for-each f (cdr xs))))
+
+;; // 한글 설명의 for-each
+;; (define (for-each proc items)
+;;   (if (null? items)
+;;       #t
+;;       (and (proc (car items))
+;;            (for-each proc (cdr items)))))
+
 
 (for-each
  (lambda (x)
