df17e5c028f3a9395680f9b6da258fc59eb5efd0
Minjun Kim <minjun@pi-lab.io>
2024-10-13 15:59:45 +0900

    append, cons, list difference

--- lisp/Ch2/list-heir.rkt
+++ lisp/Ch2/list-heir.rkt
@@ -5,3 +5,13 @@
 (car (car '((7))))
 
 (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr (car (cdr '(1(2(3(4(5(6 7))))))))))))))))))
+
+;; ---
+
+(newline)
+
+(define x (list 1 2 3))
+(define y (list 4 5 6))
+(append x y)
+(cons x y)
+(list x y)
