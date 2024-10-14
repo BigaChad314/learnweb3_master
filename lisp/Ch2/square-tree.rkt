da4f96fb71ec7eb17543bf08c843b07fa97de060
Minjun Kim <minjun@pi-lab.io>
2024-10-14 12:39:41 +0900

    tree-square higher procedure

--- lisp/Ch2/square-tree.rkt
+++ lisp/Ch2/square-tree.rkt
@@ -23,20 +23,32 @@
 ;;       (cons (square (car items))
 ;;             (square-list (cdr items)))))
 
+;; Part 1. Direct recursion
 (define (square-tree tree)
     (cond ((null? tree) '())
         ((pair? tree) (cons (square-tree (car tree))
                         (square-tree (cdr tree))))
         (else (square tree))))
 
-(define (square-tree-map x)
-  (define (square-if-number x)
-    (if (pair? x)
-        (map square-if-number x)
-        (square x)))
-  (map square-if-number x))
+;; Part 2. Map
+(define (square-tree-map tree)
+    (map (lambda (tree)
+            (if (pair? tree)
+                (square-tree-map tree)
+                (square tree)))
+        tree))
 
+;; 차수 높은 tree-map procedure
+(define (tree-map f tree)
+    (map (lambda (tree)
+        (if (pair? tree)
+            (tree-map f tree)
+            (f tree)))
+        tree))
+
+(define (square-tree-map2 tree) (tree-map square tree))
 
 (scale-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)) 10)
 (square-tree (list 1 (list 2 (list 3 4) 5) (list 6 7)))
 (square-tree-map (list 10 (list 2 (list 30 4) 50) (list 6 70)))
+(square-tree-map2 (list 1 (list 20 (list 3 40) 5) (list 60 7)))
