47ebc915ada720c13819b1ba3c3f7ae8278a9d22
Minjun Kim <minjun@pi-lab.io>
2024-10-24 13:52:41 +0900

    union set

--- lisp/Ch2/basic-set.rkt
+++ lisp/Ch2/basic-set.rkt
@@ -17,3 +17,17 @@
          (cons (car set1)
                (intersection-set (cdr set1) set2)))
         (else (intersection-set (cdr set1) set2))))
+
+(define (union-set set1 set2)
+  (cond ((null? set1) set2)
+        ((null? set2) set1)
+        ((element-of-set? (car set1) set2)
+         (union-set (cdr set1) set2))
+        (else (union-set (cdr set1) 
+                        (cons (car set1) set2)))))
+
+(adjoin-set 2 '(2 3))
+(element-of-set? 1 '(3 2 1))
+(intersection-set '(1) '(1 2))
+(union-set '(3) '(1 2))
+(union-set '(3 4 5 1) '(1 2))
