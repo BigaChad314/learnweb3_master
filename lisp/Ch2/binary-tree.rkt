2160444b9b4e5d22427997677a8c0f8da56a0093
Minjun Kim <minjun@pi-lab.io>
2024-10-25 21:29:21 +0900

    binary tree

---
+++ lisp/Ch2/binary-tree.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(define make-tree list)
+
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree elts n)
+  (if (= n 0)
+      (cons '() elts)
+      (let* ((left-size (quotient (- n 1) 2))
+             (left-result (partial-tree elts left-size))
+             (left-tree (car left-result))
+             (non-left-elts (cdr left-result))
+             (this-entry (car non-left-elts))
+             (right-size (- n (+ left-size 1)))
+             (right-result (partial-tree (cdr non-left-elts) right-size))
+             (right-tree (car right-result))
+             (remaining-elts (cdr right-result)))
+        (cons (make-tree this-entry left-tree right-tree)
+              remaining-elts))))
+
+(list->tree '(1 3 5 7 9 11))
