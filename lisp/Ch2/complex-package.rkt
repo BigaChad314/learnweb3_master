7ebd937a32080a7626f4863a13418224ecd0e64c
Minjun Kim <minjun@pi-lab.io>
2024-11-06 16:48:03 +0900

    package of rectangular and polar

--- lisp/Ch2/complex-package.rkt
+++ lisp/Ch2/complex-package.rkt
@@ -25,7 +25,31 @@
        (lambda (r a) (tag (make-from-mag-ang r a)))))
 
 (define (polar-pkg)
-  ;; Internal procedures
+  ;; Internal (closed) procedures
+  (define magnitude car)
+  (define angle cdr)
+  (define make-from-mag-ang cons)
+  (define (real-part z)
+    (* (magnitude z) (cos (angle z))))
+  (define (imag-part z)
+    (* (magnitude z) (sin (angle z))))
+  (define (make-from-real-imag x y)
+    (cons (sqrt (+ (square x) (square y)))
+          (atan y x)))
+
+  ;; Interface to the rest of the system
+  (define (tag x) (attach-tag 'polar x))
+  (put 'real-part '(polar) real-part)
+  (put 'imag-part '(polar) imag-part)
+  (put 'magnitude '(polar) magnitude)
+  (put 'angle '(polar) angle)
+  (put 'make-from-real-imag 'polar
+       (lambda (x y) (tag (make-from-real-imag x y))))
+  (put 'make-from-mag-ang 'polar
+       (lambda (r a) (tag (make-from-mag-ang r a)))))
+
+(define (polar-pkg)
+  ;; Internal(closed) procedures
   (define magnitude car)
   (define angle cdr)
   (define make-from-mag-ang cons)
