ba3db42e895ad521d8c499f9a8613a43d97e60d7
Minjun Kim <minjun@pi-lab.io>
2024-11-21 11:00:50 +0900

    lisp revamp

---
+++ lisp/Ch2/Ch 2.5 (generic op)/2.5-exercise.rkt
@@ -0,0 +1,7 @@
+#lang racket
+
+(define (complex-components-pkg)
+  (put 'real-part '(complex) real-part)
+  (put 'imag-part '(complex) imag-part)
+  (put 'magnitude '(complex) magnitude)
+  (put 'angle '(complex) angle))
